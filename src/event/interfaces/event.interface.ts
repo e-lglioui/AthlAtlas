cc87958e0501583de09bc20cabaf821c5dfb9187
e-lglioui <elgliouiyoucode@gmail.com>
2024-11-26 13:21:09 +0100

    Création de l'interface Event pour la gestion des événements sportifs

--- src/event/interfaces/event.interface.ts
+++ src/event/interfaces/event.interface.ts
@@ -1,1 +1,11 @@
-import {Event} from '../schemas/event.schema'
+import {Event} from '../schemas/event.schema';
+import { CreateEventDto } from '../dtos/create-event.dto';
+import { UpdateEventDto } from '../dtos/update-event.dto';
+
+ export interface IEventService {
+getAllEvent(): Promise<Event[]>;
+findById(id: string): Promise<Event>;
+findByName(name: string): Promise<Event>;
+createEvent(CreateEventDto: CreateEventDto): Promise<Event>;
+updateEvent(eventId: string, UpdateEventDto: UpdateEventDto): Promise<Event>;
+ }
