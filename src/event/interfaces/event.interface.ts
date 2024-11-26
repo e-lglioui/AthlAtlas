9386e0bf3e6e16ed3c65f7da6330279e263a8d44
e-lglioui <elgliouiyoucode@gmail.com>
2024-11-26 15:29:44 +0100

    feat(event): implement exception handling and service enhancements
    - Added event-specific exceptions in event.exception.ts to handle validation and error scenarios.
    - Updated event service interface in event.interface.ts to align with the repository.
    - Introduced event.service.ts and its corresponding unit test event.service.spec.ts for event service logic.
    - Modified user registration HTTP endpoint logic in user.register.endpoint.http to include necessary adjustments.

--- src/event/interfaces/event.interface.ts
+++ src/event/interfaces/event.interface.ts
@@ -4,7 +4,7 @@ import { UpdateEventDto } from '../dtos/update-event.dto';
 
  export interface IEventService {
 getAllEvent(): Promise<Event[]>;
-findById(id: string): Promise<Event>;
+findById(eventId: string): Promise<Event>;
 findByName(name: string): Promise<Event>;
 createEvent(CreateEventDto: CreateEventDto): Promise<Event>;
 updateEvent(eventId: string, UpdateEventDto: UpdateEventDto): Promise<Event>;
