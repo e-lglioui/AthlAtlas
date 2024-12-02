5499fd3f2512ff28aea30d74431c21fc1b29dc98
e-lglioui <elgliouiyoucode@gmail.com>
2024-12-02 10:17:26 +0100

    dockerise application

--- Dockerfile
+++ Dockerfile
@@ -17,11 +17,17 @@ FROM node:20-alpine
 WORKDIR /app/
 
 ARG NODE_ENV=production
+ARG JWT_SECRET
+ARG MONGODB_URI
 ENV NODE_ENV=${NODE_ENV}
+ENV JWT_SECRET=${JWT_SECRET}
+ENV MONGODB_URI=${MONGODB_URI}
 
 COPY --from=build /app/dist ./dist
 
 COPY --from=build /app/package.json ./
+
+# COPY --from=build /app/.env ./.env
 
 RUN npm install --only=production
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'; 
 async function bootstrap() {
@@ -11,6 +12,27 @@ async function bootstrap() {
     credentials: true
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Sport App API')
+    .setDescription('The Sport App API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
